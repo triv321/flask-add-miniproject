8397005a1999f9f7c9e82b0818608aad0eac7d6b
Bharat Thakur <equinoxter707@gmail.com>
2025-07-18 14:44:55 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,16 @@
+Production Ready Dockerized Flask API
+
+## Run with Docker
+'''bash
+docker build -t flask-api-prod .
+docker run -p 5000:5000 flask-api-prod
+
+## Run with Docker Compose
+docker compose up --build
+
+## Test
+In another terminal, run:
+
+curl.exe -X POST http://localhost:5000/add -H "Content-Type: application/json" -d "{\"num1\":x,\"num2\":y}"
+# where x and y are numbers
+
---
+++ app/app.py
@@ -0,0 +1,26 @@
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "Hello from Calculator Backend"
+
+@app.route("/add", methods=["POST"])
+def add():
+    data = request.get_json()
+    num1 = data.get("num1")
+    num2 = data.get("num2")
+
+    if num1 is None or num2 is None:
+        return jsonify({"error": "Missing input"}), 400
+
+    try:
+        result = float(num1) + float(num2)
+    except (KeyError, TypeError, ValueError):
+        return jsonify({"error": "Invalid Input"}), 400
+
+    return jsonify({"result": result}), 200
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
